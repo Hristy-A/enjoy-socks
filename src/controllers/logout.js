a03c6714e3750099cfbd4a303fe8f25ae5fcad17
Hristy <curtov.artyom@yandex.ru>
2022-11-30 22:17:51 +0500

    change hardcode to constants and env variables

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -7,7 +7,7 @@ module.exports = function register(logoutRoute) {
       req.session.destroy((error) => {
         if (error) next(error);
 
-        res.clearCookie('user_id');
+        res.clearCookie(req.app.locals.SESSION_COOKIE_NAME);
         res.redirect('/');
       });
     });
