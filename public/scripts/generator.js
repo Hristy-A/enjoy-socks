6476a049c0ec5a52d0e41cc97f1d9e767767de38
Hristy <curtov.artyom@yandex.ru>
2022-12-01 01:46:30 +0500

    Add save data to local storage

--- public/scripts/generator.js
+++ public/scripts/generator.js
@@ -1,6 +1,10 @@
 const generatorBox = document.querySelector('.cst-generator-box');
 const sockPalettes = document.querySelectorAll('.cst-select-parent');
 
+const generatorImage = generatorBox.querySelector('.cst-image');
+const generatorPattern = generatorBox.querySelector('.cst-pattern');
+const generatorColor = generatorBox.querySelector('.cst-color');
+
 sockPalettes.forEach((sockPalette) => {
   console.log('tut', sockPalette);
   sockPalette.addEventListener('click', (event) => {
@@ -17,4 +21,24 @@ sockPalettes.forEach((sockPalette) => {
     layer.src = target.dataset.src;
     layer.dataset.id = target.dataset.id;
   });
+});
+
+function formSock() {
+  return {
+    colorId: generatorColor.dataset.id,
+    imageId: generatorImage.dataset.id || null,
+    patternId: generatorPattern.dataset.id || null,
+    price: 990, //! hardcode
+    size: 'L-XXL', //! hardcode
+  };
+}
+
+const addFavoriteBtn = document.getElementById('add-favorite-btn');
+const addCartBtn = document.getElementById('add-cart-btn');
+
+addFavoriteBtn.addEventListener('click', (event) => {
+  addFavorite(formSock());
+});
+addCartBtn.addEventListener('click', (event) => {
+  addCart(formSock());
 });
