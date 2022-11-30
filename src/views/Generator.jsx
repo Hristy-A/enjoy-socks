8ca8835d1360a0c13764d2c063d8c28c5cf231d4
Aleksei Novikov <resan.konstiga@gmail.com>
2022-11-30 17:19:50 +0300

    Generator Images Done

--- src/views/Generator.jsx
+++ src/views/Generator.jsx
@@ -8,15 +8,40 @@ module.exports = function Home({ user }) {
         <img className="cst-home-element" src="./images/logo.png" alt="" width="350" height="350" />
       </div>
       <div>
+
         <div>
-          <div>Color:</div>
-          <div>Image: </div>
-          <div>Patern: </div>
+          <div>
+            Цвет:
+            <img src="/images/socks/colors/white.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/colors/blue.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/colors/green.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/colors/pink.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/colors/purple.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/colors/yellow.png" className="card-img-top cst-generator-btn" alt="" />
+          </div>
+
+          <div>
+            Рисунок:
+            <img src="/images/socks/images/1.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/images/2.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/images/3.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/images/4.png" className="card-img-top cst-generator-btn" alt="" />
+          </div>
+
+          <div>
+            Узор:
+            <img src="/images/socks/patterns/1.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/patterns/2.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/patterns/3.png" className="card-img-top cst-generator-btn" alt="" />
+            <img src="/images/socks/patterns/4.png" className="card-img-top cst-generator-btn" alt="" />
+          </div>
         </div>
+
         <div>
           <button type="submit">В избранное</button>
           <button type="submit">В избранное</button>
         </div>
+
       </div>
     </Layout>
   );
