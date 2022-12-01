b342c11b5db070ed9c3667ac1d4b38108c1f1f0c
Aleksei Novikov <resan.konstiga@gmail.com>
2022-12-01 16:08:19 +0300

    Visual Bug Fix

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -9,7 +9,6 @@ module.exports = function Layout({ children, user, styles = '' }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="./styles/common.css" />
         <link rel="stylesheet" href="./styles/style.css" />
         { styles && <link rel="stylesheet" href={styles} /> }
         <title>Enjoy Socks</title>
@@ -19,8 +18,8 @@ module.exports = function Layout({ children, user, styles = '' }) {
       <body>
         <div className="cst-wrapper">
           <header className="cst-header">
-            <nav className="navbar navbar-expand-lg bg-light">
-              <div className="container-fluid d-flex">
+            <nav className="navbar navbar-expand-lg bg-light cst-navbar">
+              <div className="container-fluid d-flex cst-navbar">
                 <span className="pb-2">
                   <a className="navbar-brand" href="./">
                     <img src="./images/logo.png" alt="" width="50" height="50" className="d-inline-block align-text-top cst-logo" />
@@ -34,6 +33,20 @@ module.exports = function Layout({ children, user, styles = '' }) {
                 <div className="collapse navbar-collapse" id="navbarNav">
                   <div className="flex-grow-1" />
                   <ul className="navbar-nav justify-content-end">
+
+                    <li className="nav-item text-center">
+                      <a className="nav-link" href="/generator">
+                        <div>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                          </svg>
+                        </div>
+                        <div>
+                          Создать
+                        </div>
+                      </a>
+                    </li>
 
                     <li className="nav-item text-center">
                       <a className="nav-link" href="/favorites">
