50777ca88f823424b7218c639b4c2bbe4d93fa33
Aleksei Novikov <resan.konstiga@gmail.com>
2022-12-01 17:39:04 +0300

    Test Mailer Done

---
+++ mail-test.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+async function sendMail() {
+  // const testEmailAccount = await nodemailer.createTestAccount();
+  // console.log(testEmailAccount);
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mail.ru',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'enjoysocks@mail.ru',
+      pass: 'HtvnjWZh6mXas512QAbi',
+    },
+  });
+
+  const result = await transporter.sendMail({
+    from: 'Enjoy Socks <enjoysocks@mail.ru>',
+    to: 'curtov.artyom@yandex.ru, curtov.artyom@yandex.ru',
+    subject: 'Message from Node js',
+    text: 'This message was sent from Node js server.',
+    html:
+    'This <i>message</i> was sent from <strong>Node js</strong> server.',
+  });
+
+  console.log(result);
+}
+
+sendMail();
