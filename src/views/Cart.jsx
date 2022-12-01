d9c4d5633dac4385c8df6b820a33f02c7e1ac32a
Max_V <mdzen87@gmail.com>
2022-12-01 19:16:14 +0300

    views 5

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -58,25 +58,23 @@ module.exports = function Cart({ user, socks }) {
 
           {arr.map((el) => (
             <div className="card body-cart">
-
+              <div className="card-body d-flex justify-content-center cst-delete-btn">
+                <div className="nav-item text-center ">
+                  <a className="nav-link" href="/generator">
+                    <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
+                      </svg>
+                    </div>
+                  </a>
+                </div>
+              </div>
               <div className="d-flex justify-content-center">
 
                 <div className="cst-favorits-box">
                   <img className="cst-sock cst-image favorits" src={el.image.link} alt="" />
                   <img className="cst-sock cst-pattern favorits" src={el.pattern.link} alt="" />
                   <img className="cst-sock cst-color favorits" src={el.color.link} alt="" />
-                </div>
-
-              </div>
-              <div className="card-body d-flex justify-content-center">
-                <div className="nav-item text-center">
-                  <a className="nav-link" href="/generator">
-                    <div>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-x-circle-fill" viewBox="0 0 16 16">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z" />
-                      </svg>
-                    </div>
-                  </a>
                 </div>
               </div>
             </div>
