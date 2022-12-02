3b73cee610eb59275ca888b18f8c0d98eee0895d
Hristy <curtov.artyom@yandex.ru>
2022-12-02 13:25:57 +0500

    fix merge bug

--- src/views/Cart.jsx
+++ src/views/Cart.jsx
@@ -40,24 +40,25 @@ module.exports = function Cart({ user, socks }) {
 
       <div className="body">
         <div data-card-container className="d-flex flex-wrap justify-content-center">
-        {socks.length === 0 && (
-        <div className="d-flex flex-column justify-content-center align-items-center">
-          <div>
-            <h3>
-              В корзине пусто
-            </h3>
+          {socks.length === 0 && (
+          <div className="d-flex flex-column justify-content-center align-items-center">
+            <div>
+              <h3>
+                В корзине пусто
+              </h3>
+            </div>
+            <div>
+              <h5>
+                Добавляйте товары с помощью
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-basket2" viewBox="0 0 16 16">
+                  <path d="M4 10a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 1 1 2 0v2a1 1 0 0 1-2 0v-2z" />
+                  <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-.623l-1.844 6.456a.75.75 0 0 1-.722.544H3.69a.75.75 0 0 1-.722-.544L1.123 8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.163 8l1.714 6h8.246l1.714-6H2.163z" />
+                </svg>
+              </h5>
+            </div>
           </div>
-          <div>
-            <h5>
-              Добавляйте товары с помощью
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-basket2" viewBox="0 0 16 16">
-                <path d="M4 10a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 1 1 2 0v2a1 1 0 0 1-2 0v-2z" />
-                <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-.623l-1.844 6.456a.75.75 0 0 1-.722.544H3.69a.75.75 0 0 1-.722-.544L1.123 8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.163 8l1.714 6h8.246l1.714-6H2.163z" />
-              </svg>
-            </h5>
-          </div>
+          )}
         </div>
-        )}
 
         <div className="d-flex flex-wrap justify-content-center">
           {socks.map((sock) => (
